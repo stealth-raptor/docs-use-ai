b5fd0fcb754419806e3f1ce49314dc7ded17c761
Aryan23Sethi <aryansethi2311@gmail.com>
2025-06-23 00:45:46 +0530

    Clever engg

--- app/api/ai-model/route.js
+++ app/api/ai-model/route.js
@@ -34,7 +34,7 @@ export async function POST(req) {
 
         const openai = new OpenAI({
             baseURL: "https://openrouter.ai/api/v1",
-            apiKey: process.env.OPENROUTER_API_KEY,
+            apiKey: process.env.OPENROUTER_API_KEY_2,
 
         })
 
