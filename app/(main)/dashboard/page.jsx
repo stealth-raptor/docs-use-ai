ca265b0bf741658239ba6aff6c3a5de8de46dbb4
Aryan23Sethi <aryansethi2311@gmail.com>
2025-08-19 16:04:49 +0530

    playing with clarity

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,17 +1,24 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Welcome from './_components/Welcome'
 import CreateOptions from './_components/CreateOptions'
 import DiagnosisList from './_components/DiagnosisList'
+import Clarity from '@microsoft/clarity';
+
 
 
 const Dashboard = () => {
+
+  useEffect(() => {
+    Clarity.event("custom-event");
+    console.log("Clarity event logged");
+  }, [])
   return (
     <div>
-      <Welcome/>
+      <Welcome />
       <h2 className='my-3 font-bold text-2xl'>Dashboard</h2>
-      <CreateOptions/>
-      <DiagnosisList/>
+      <CreateOptions />
+      <DiagnosisList />
     </div>
   )
 }
